e051af9471aec3d4a46fd96f268ddbd281c24412
Seikou <seikou615@gmail.com>
2022-09-23 17:34:01 +0100

    added type for children, added some default styles

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -3,5 +3,10 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   *,*::before,*::after {
     box-sizing: border-box;
+    padding: 0;
+    margin: 0;
   }
+
+
+
 `;
