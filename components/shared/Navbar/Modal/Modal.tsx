93e8bb92ba41139c508229cb74cc0cf3ab1d0729
Seikou <seikou615@gmail.com>
2022-09-24 10:45:04 +0100

    added basic navbar animation

--- components/shared/Navbar/Modal/Modal.tsx
+++ components/shared/Navbar/Modal/Modal.tsx
@@ -4,18 +4,36 @@ import NavItems from '../NavItems/NavItems'
 import React from 'react'
 
 interface PropTypes {
-    buttons: JSX.Element;
+  buttons: JSX.Element
+  setOpen: Function
 }
 
-const Modal = ({ buttons }: PropTypes) => {
-    return (
-        <StyledModal>
-            <Container>
-                <NavItems />
-                {buttons}
-            </Container>
-        </StyledModal>
-    )
+const animation = {
+  hidden: {
+    top: '-100vh',
+  },
+
+  visible: {
+    top: 0,
+    transition: { duration: .5 },
+  },
+
+  exit: {
+    top: '-100vh',
+    transition: { duration: .5 },
+    
+  },
+}
+
+const Modal = ({ buttons, setOpen }: PropTypes) => {
+  return (
+    <StyledModal variants={animation} initial="hidden" animate="visible" exit="exit">
+      <Container>
+        <NavItems setOpen={setOpen} />
+        {buttons}
+      </Container>
+    </StyledModal>
+  )
 }
 
 export default Modal
--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -22,19 +22,18 @@ const Navbar = () => {
   const [open, setOpen] = useState(false)
   const title = open ? 'Close navigation' : 'Open navigation'
 
-
+  const handleNavClick = () => setOpen(false);
 
   const buttons = (
     <>
-      <Link href="/login">
-        <LoginButton>Sign in</LoginButton>
-      </Link>
       <Link href="/signup">
-        <RegisterButton>Create Account</RegisterButton>
+        <RegisterButton onClick={() => setOpen(false)}>Create Account</RegisterButton>
+      </Link>
+      <Link href="/login">
+        <LoginButton onClick={() => setOpen(false)}>Sign in</LoginButton>
       </Link>
     </>
   )
-
 
   return (
     <>
@@ -57,14 +56,9 @@ const Navbar = () => {
           </NavItemsWrapper>
         </NavContainer>
       </StyledNavbar>
-      <AnimatePresence>
-      {open && <Modal buttons={buttons} />}
-      </AnimatePresence>
+      <AnimatePresence>{open && <Modal setOpen={setOpen} buttons={buttons} />}</AnimatePresence>
     </>
   )
 }
 
 export default Navbar
-
-
-
